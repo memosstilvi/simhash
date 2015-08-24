27de74a7a64c47020deddeea9bb04f871ee4bcb9
Menelaos Kokolios <kokolios8@gmail.com>
2015-08-24 16:21:30 +0100

    Refactor code and add README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+##Simhash made simple
+
+####A simple python module that calculates calculates the Simhash value of documents.
+
+Simhash is a hashing technique that belongs to the LSH (Local Sensitive Hashing) algorithmic family.
+It was initially developed by Moses S. Charikar in 2002 and is described in detail in his [paper] (http://www.cs.princeton.edu/courses/archive/spring04/cos598B/bib/CharikarEstim.pdf).
+The main goal of this algorithm is to detect near duplicate documents, since similar documents will most probably 
+have similar (or even the same) simhash value.
+A straightforward explanation of the Simhash algorithm can be found [here] (http://matpalm.com/resemblance/simhash/).
+
+###How it works
+
+The unique and most interesting characteristic of the Simhash algorithm is that near duplicate documents will most likely 
+have the same (or pretty similar) hash value. This feature makes it very useful when it comes to near duplicate document detection 
+since it doesn’t require to compare all the documents with each other. That comparison would result to a O(n^2) complexity.
+By using simhash we no longer need a pair-wise comparison between all the documents. We only need a single pass over our collection. 
+After calculating the hash values for all the documents we just need to compare the ones that share the same (or similar) hash values which results in an O(n) complexity.
+
+More specifically, in order to calculate the simhash value of a document we perform the following steps:
+1. Split the document in tokens (words, characters or n-grams)
+2. Hash each token separately using MD5
+3. Calculate the bit representation of this MD5 hashes
+4. Calculate and apply a weight to each of the document’s tokens. In our case this weights are the term frequencies in the document.
+5. Merge the bit representations of all the tokens of the document in order to calculate the document’s final hash value.
+
+###Quick Use (from stdin)
+
+`cat YOUR_DOC | python simhash.py`
+
+###License
+
+Distributed under the MIT license. See `LICENSE` for more information.
---
+++ simhash.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+from hashlib import md5
+import sys
+
+class Token:
+
+    def __init__(self, hash_list, weight):
+        self.hash_list = hash_list
+        self.weight = weight
+
+def tokenize(doc):
+    """
+    Lower case, remove punctuation and split in spaces
+    """
+    doc = doc.lower()
+    doc = re.sub('[,;]', ' ', doc)
+    doc = re.split('\s+', doc)
+    doc = filter(None, doc)
+    return doc
+
+def md5Hash(token):
+    h = bin(int(md5(token).hexdigest(), 16))
+    return h[2:]
+
+def hash_threshold(token_dict, fp_len):
+    """
+    Iterate through the token dictionary multiply the hash lists with the weights
+    and apply the binary threshold
+    """
+    sum_hash = [0] * fp_len
+    for _, token in token_dict.iteritems():
+        sum_hash = [ x + token.weight * y for x, y in zip(sum_hash, token.hash_list)]
+
+    # apply binary threshold
+    for i, ft in enumerate(sum_hash):
+        if ft > 0:
+            sum_hash[i] = 1
+        else:
+            sum_hash[i] = 0
+    return sum_hash
+
+def binconv(fp, fp_len):
+    """
+    Converts 0 to -1 in the tokens' hashes to facilitate
+    merging of the tokens' hashes later on.
+    input  : 1001...1
+    output : [1,-1,-1, 1, ... , 1]
+    """
+    vec = [1] * fp_len
+    for indx, b in enumerate(fp):
+        if b == '0':
+            vec[indx] = -1
+    return vec
+
+
+def calc_weights(terms, fp_len):
+    """
+    Calculates the weight of each one of the tokens. In this implementation
+    these weights are equal to the term frequency within the document.
+
+    :param tokens: A list of all the tokens (words) within the document
+    :fp_len: The length of the Simhash values
+    return dictionary "my_term": Token([-1,1,-1,1,..,-1], 5)
+    """
+    term_dict = {}
+    for term in terms:
+        # get weights
+        if term not in term_dict:
+            fp_hash = md5Hash(term).zfill(fp_len)
+            fp_hash_list = binconv(fp_hash, fp_len)
+            token = Token(fp_hash_list, 0)
+            term_dict[term] = token
+        term_dict[term].weight += 1
+    return term_dict
+
+def simhash(doc, fp_len=128):
+    """
+    :param doc: The document we want to generate the Simhash value
+    :fp_len: The number of bits we want our hash to be consisted of.
+                Since we are hashing each token of the document using
+                md5 (which produces a 128 bit hash value) then this
+                variable fp_len should be 128. Feel free to change
+                this value if you use a different hash function for
+                your tokens.
+    :return The Simhash value of a document ex. '0000100001110'
+    """
+    tokens = tokenize(doc)
+    token_dict = calc_weights(tokens, fp_len)
+    fp_hash_list = hash_threshold(token_dict, fp_len)
+    fp_hast_str =  ''.join(str(v) for v in fp_hash_list)
+    return fp_hast_str
+
+
+if __name__ == '__main__':
+    # Just for demonstration
+    doc = sys.stdin.read()
+    binary_hash = simhash(doc)
+    print binary_hash
